46685d0ffa9556a8f1b1e3ca54506dc507427e48
rongvang17 <nguyenminhthanh0917@gmail.com>
2024-08-19 11:24:33 +0700

    upload

--- inference2.py
+++ inference2.py
@@ -3,6 +3,7 @@ import numpy as np
 import torch
 import json
 import math
+import time
 
 from boxmot import DeepOCSORT
 from pathlib import Path
@@ -10,23 +11,27 @@ from absl import app, flags
 from absl.flags import FLAGS
 from shapely.geometry import Point, Polygon
 from collections import defaultdict, deque
+
 # from models.common import DetectMultiBackend, AutoShape
 
-flags.DEFINE_string('ccfg', './cfg/cfg2.txt', 'path to camera config file')
+flags.DEFINE_string('ccfg', './cfg/cfg2.txt', 'Path to camera config file')
 
-# detection model
+# Detection model
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = torch.hub.load("yolov5", "custom",
-                       path="/home/minhthanh/directory_env/object_tracking/yolov5/weights/best.pt",
-                       source="local")  # local repo
-model.conf = 0.4
+model = torch.hub.load(
+    "yolov5", 
+    "custom",
+    path="/home/minhthanh/directory_env/object_tracking/yolov5/weights/best.pt",
+    source="local"  # Local repo
+)
+model.conf = 0.25
 model.max_det = 1000
 model.eval()
 # model.cuda()
 
-# tracking model
+# Tracking model
 tracker = DeepOCSORT(
-    model_weights=Path('yolov5/osnet_x0_25_msmt17.pt'), # which ReID model to use
+    model_weights=Path('yolov5/osnet_x0_25_msmt17.pt'),  # Which ReID model to use
     device='cpu',
     fp16=False,
 )
@@ -44,7 +49,7 @@ class ViewTransformer:
 
         reshaped_points = points.reshape(-1, 1, 2).astype(np.float32)
         transformed_points = cv2.perspectiveTransform(
-                reshaped_points, self.m)
+                                 reshaped_points, self.m)
         return transformed_points.reshape(-1, 2)
 
 
@@ -55,8 +60,8 @@ def draw_box_2(
         distance_cal,
         num_frame,
         area_cal,
-        SOURCE,
-        TARGET
+        source,
+        target
 ):
     global colors
     global class_names
@@ -65,81 +70,70 @@ def draw_box_2(
     global speed_function
     global fps
     global coordinates
-    xyxys = tracks[:, 0:4].astype('int')
-    ids = tracks[:, 4].astype('int')
+    global view_transformer
+
+    xyxys = tracks[:, 0:4].astype(int)
+    ids = tracks[:, 4].astype(int)
     conf = tracks[:, 5]
-    clss = tracks[:, 6].astype('int')
-    inds = tracks[:, 7].astype('int') # float64 to int
-    num_car = 0
-    num_bike = 0
-    num_bus = 0
-    num_truck = 0
+    clss = tracks[:, 6].astype(int)
+    inds = tracks[:, 7].astype(int)  # float64 to int
+
+    num_car, num_bike, num_bus, num_truck = 0, 0, 0, 0
     avg_speed = 0
 
-    view_transformer = ViewTransformer(source=SOURCE, target=TARGET)
-
     for idx in range(tracks.shape[0]):
-        left , top, right, bottom = xyxys[idx]
+        left, top, right, bottom = xyxys[idx]
         x_center, y_center = (left + right) / 2, (top + bottom) / 2
         point = Point(x_center, y_center)
         is_inside = roi_polygon.contains(point)
 
         if is_inside:
-
-            # plug the view transformer into an existing detection pipeline
-            coord_object = []
-            coord_object.append((x_center, y_center))
+            coord_object = [(x_center, y_center)]
             coord_object = np.array(coord_object)
             points = view_transformer.transform_points(points=coord_object).astype(int)
 
-            # store the transformed coordinates
+            # Store the transformed coordinates
             coordinates[ids[idx]].append((points[0][0], points[0][1], num_frame, -1))
-            
-            # wait to have enough data
-            if len(coordinates[ids[idx]]) >= (4*fps):
-                # calculate the speed
+
+            # Wait to have enough data
+            if len(coordinates[ids[idx]]) >= (4 * fps):
+                # Calculate the speed
                 coordinate_start = coordinates[ids[idx]][-1]
-                coordinate_end = coordinates[ids[idx]][2*fps]
-                distance = ((coordinate_start[0] - coordinate_end[0])**2 + 
+                coordinate_end = coordinates[ids[idx]][2 * fps]
+                distance = ((coordinate_start[0] - coordinate_end[0])**2 +
                             (coordinate_start[1] - coordinate_end[1])**2)**0.5
                 eta = abs(coordinate_start[2] - coordinate_end[2])
-                if distance>=0.5:
-                    time = float(abs(eta) / fps)
+                if distance >= 0.5:
+                    time = float(eta / fps)
                     speed = (distance / time) * 3.6
                     vex = coordinates[ids[idx]]
                     coordinates[ids[idx]][-1] = (points[0][0], points[0][1], num_frame, speed)
                     avg_speed += (speed / len(tracks))
+
                 vex = coordinates[ids[idx]][-1]
                 check_speed = vex[3]
                 if int(check_speed) != -1:
-                    cv2.putText(img, "{:.2f}".format(check_speed), (left + 2, top - 2), 
-                                cv2.FONT_HERSHEY_SIMPLEX, 1, colors[clss[idx]], 2)
-                # cv2.putText(img, "{}_{}".format(points[0][0], points[0][1]), 
-                #             (left + 2, top - 2), cv2.FONT_HERSHEY_SIMPLEX, 1, colors[clss[idx]], 2)
+                    cv2.putText(img, "{:.2f}".format(check_speed), (left + 2, top - 2),
+                                cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
 
-            text_conf = conf[idx]
             text_clss = clss[idx]
-            text_ids = ids[idx]
-            # text_inds = inds[idx]
             if text_clss == 0:
                 num_car += 1
-            if text_clss == 1:
+            elif text_clss == 1:
                 num_bike += 1
-            if text_clss == 2:
+            elif text_clss == 2:
                 num_bus += 1
-            if text_clss == 3:
+            elif text_clss == 3:
                 num_truck += 1
 
-            cv2.rectangle(img, (left, top), (right, bottom), colors[clss[idx]], 2)
-    
+            cv2.rectangle(img, (left, top), (right, bottom), colors[clss[idx]], 1)
+
     occupancies = area_cal(num_car, num_bike, num_bus, num_truck)
     if avg_speed < 5 and occupancies > 0.85:
         cv2.putText(img, 'TAC DUONG', (350, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
     else:
         cv2.putText(img, 'KHONG TAC', (350, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3)
-
-    # cv2.putText(img, 'num_bus:{}'.format(num_bus), (600, 65), cv2.FONT_HERSHEY_SIMPLEX, 1, colors[2], 2)
-    # cv2.putText(img, 'num_truck:{}'.format(num_truck), (600, 85), cv2.FONT_HERSHEY_SIMPLEX, 1, colors[3], 2)    
+    
 
 
 def draw_box(img, tracks, distance_cal, num_frame, area_cal):
@@ -149,19 +143,18 @@ def draw_box(img, tracks, distance_cal, num_frame, area_cal):
     global track_object
     global speed_function
     global fps
-    num_car = 0
-    num_bike = 0
-    num_bus = 0
-    num_truck = 0
+
+    num_car, num_bike, num_bus, num_truck = 0, 0, 0, 0
     avg_speed = 0
-    xyxys = tracks[:, 0:4].astype('int')
-    ids = tracks[:, 4].astype('int')
+
+    xyxys = tracks[:, 0:4].astype(int)
+    ids = tracks[:, 4].astype(int)
     conf = tracks[:, 5]
-    clss = tracks[:, 6].astype('int')
-    inds = tracks[:, 7].astype('int') # float64 to int
+    clss = tracks[:, 6].astype(int)
+    inds = tracks[:, 7].astype(int)  # float64 to int
 
     for idx in range(tracks.shape[0]):
-        left , top, right, bottom = xyxys[idx]
+        left, top, right, bottom = xyxys[idx]
         x_center, y_center = (left + right) / 2, (top + bottom) / 2
         point = Point(x_center, y_center)
         is_inside = roi_polygon.contains(point)
@@ -175,9 +168,10 @@ def draw_box(img, tracks, distance_cal, num_frame, area_cal):
                 pivot = top
                 new_distance = to[0]
                 distance = distance_cal(pivot)
+
                 if to[0] != -1:
-                    delta_distance = abs(distance-new_distance)
-                    if delta_distance >= 4.0 or num_frame-his_fram > 2*fps:
+                    delta_distance = abs(distance - new_distance)
+                    if delta_distance >= 4.0 or num_frame - his_fram > 2 * fps:
                         object_speed = speed_function(delta_distance, to[1], num_frame)
                         his_fram = num_frame
                         new_distance = distance
@@ -188,40 +182,45 @@ def draw_box(img, tracks, distance_cal, num_frame, area_cal):
                     track_object[ids[idx]] = [new_distance, his_fram, object_speed]
 
                 if object_speed != -1:
-                    avg_speed += (object_speed/len(track_object))
-                    cv2.putText(img, "%.2f" % (object_speed), (left + 5, top - 8), 
-                                cv2.FONT_HERSHEY_SIMPLEX, 1, colors[clss[idx]], 2)
-                                    
+                    avg_speed += object_speed / len(track_object)
+                    cv2.putText(
+                        img,
+                        "%.2f" % object_speed,
+                        (left + 5, top - 8),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        1,
+                        colors[clss[idx]],
+                        2,
+                    )
+
             else:
                 track_object[ids[idx]] = [-1, num_frame, -1]
 
-            text_conf = conf[idx]
             text_clss = clss[idx]
-            text_ids = ids[idx]
-            # text_inds = inds[idx]
             if text_clss == 0:
                 num_car += 1
-            if text_clss == 1:
+            elif text_clss == 1:
                 num_bike += 1
-            if text_clss == 2:
+            elif text_clss == 2:
                 num_bus += 1
-            if text_clss == 3:
+            elif text_clss == 3:
                 num_truck += 1
-            
-            label = "{}".format(text_ids)
+
+            label = "{}".format(ids[idx])
             cv2.rectangle(img, (left, top), (right, bottom), colors[text_clss], 2)
             # cv2.putText(img, label, (left + 5, top - 8), cv2.FONT_HERSHEY_SIMPLEX, 1, colors[text_clss], 2)
 
     occupancies = area_cal(num_car, num_bike, num_bus, num_truck)
     if avg_speed < 5 and occupancies > 0.8:
-        cv2.putText(img, 'TAC DUONG', (350, 350), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+        cv2.putText(img, "TAC DUONG", (350, 350), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
     else:
-        cv2.putText(img, 'KHONG TAC', (350, 350), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3)
+        cv2.putText(img, "KHONG TAC", (350, 350), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3)
 
-    cv2.putText(img, 'num_car:{}'.format(num_car), (630, 130), cv2.FONT_HERSHEY_SIMPLEX, 1, colors[0], 2)
-    cv2.putText(img, 'num_bike:{}'.format(num_bike), (630, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, colors[1], 2)
+    cv2.putText(img, "num_car:{}".format(num_car), (630, 130), cv2.FONT_HERSHEY_SIMPLEX, 1, colors[0], 2)
+    cv2.putText(img, "num_bike:{}".format(num_bike), (630, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, colors[1], 2)
     # cv2.putText(img, 'num_bus:{}'.format(num_bus), (600, 65), cv2.FONT_HERSHEY_SIMPLEX, 1, colors[2], 2)
-    # cv2.putText(img, 'num_truck:{}'.format(num_truck), (600, 85), cv2.FONT_HERSHEY_SIMPLEX, 1, colors[3], 2)    
+    # cv2.putText(img, 'num_truck:{}'.format(num_truck), (600, 85), cv2.FONT_HERSHEY_SIMPLEX, 1, colors[3], 2)
+    
 
 
 def get_pixel(rw, ppa, aw, delta, rh, vh, vw, diameter,  isLeft = False):
@@ -266,8 +265,10 @@ def get_roi_points(
     ppa
 ):
     width_angle = h_angle * math.pi / 180
-    point_left = get_pixel(left_real_width, ppa, width_angle, touch_angle, real_height, height, width, diameter, True)
-    point_right = get_pixel(right_real_width, ppa, width_angle, touch_angle, real_height, height, width, diameter,
+    point_left = get_pixel(left_real_width, ppa, width_angle, touch_angle, 
+                           real_height, height, width, diameter, True)
+    point_right = get_pixel(right_real_width, ppa, width_angle, touch_angle, 
+                            real_height, height, width, diameter,
                             False)
     lst = [point_left[0], point_left[1], point_right[1]]
     t1 = point_left[0][1]
@@ -291,7 +292,7 @@ def get_roi_points(
 
 
 def read_ccfg(height, width, diameter):
-    file_path = FLAGS.ccfg
+    # file_path = FLAGS.ccfg
     file_path = './cfg/cfg.txt'
 
     # print('use camera config file: ' + file_path)
@@ -308,7 +309,9 @@ def read_ccfg(height, width, diameter):
     ppa = 0.5*height/delta_angle
     app = 2*delta_angle/height
     touch_angle = abs(base_angle-delta_angle)
-    roi_points, diameter = get_roi_points(touch_angle, h_angle, real_height, left_width, right_width, width, height, diameter, ppa)
+    roi_points, diameter = get_roi_points(touch_angle, h_angle, real_height, 
+                                          left_width, right_width, width, height, 
+                                          diameter, ppa)
     left_area = diameter*left_width
     right_area = diameter*right_width
 
@@ -344,100 +347,104 @@ def occupancy_estimation(car, bike, bus, truck, area):
     return lambda x, y, z, h: (x * car + y * bike + z * bus + h * truck) / area
 
 
-def main(_argv):
-
+def main():
     # video_path = '/home/minhthanh/Downloads/a7_non_hd_front_normal_23s.mp4'
     video_path = '/home/minhthanh/Downloads/video1_2.mp4'
     vid = cv2.VideoCapture(video_path)
     width = int(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    
     global fps
     fps = int(vid.get(cv2.CAP_PROP_FPS))
+    
     conf_threshold = 0.25
-    tracking_class = [0, 1, 2, 3] # 0:car, 1:bike, 2:bus, 3:truck
-    global colors
-    global class_names
-    colors = [(255, 144, 30), (0, 255, 255), (255, 0, 0), (255, 255, 0), (0, 255, 255)]
+    tracking_class = [0, 1, 2, 3]  # 0:car, 1:bike, 2:bus, 3:truck
+    
+    global colors, class_names
+    colors = [(153, 0, 76), (0, 255, 255), (255, 0, 0), (255, 255, 0), (0, 255, 255)]
+    
     with open("yolov5/data_txt/classes.names") as f:
         class_names = f.read().strip().split('\n')
 
     diameter = 30
     distance_cal, roi_points, left_area, right_area = read_ccfg(height, width, diameter)
     roi_points, raw_rois = region_of_interested(roi_points, width, height)
-    # print(roi_points)
     top, bottom, right, left = search_pivot(roi_points)
+    
     global roi_polygon
     roi_polygon = Polygon(roi_points)
 
-    # save infor object: (distane, frame, speed)
+    # Save object information: (distance, frame, speed)
     global track_object
     track_object = dict()
 
     global speed_function
     speed_function = speed_estimation(fps=fps)
+    
     num_frame = 0
 
-    # save object information in 4s (x, y, frame)
+    # Save object coordinates for 4 seconds (x, y, frame)
     global coordinates
-    coordinates = defaultdict(lambda: deque(maxlen=4*fps))
+    coordinates = defaultdict(lambda: deque(maxlen=4 * fps))
 
     true_area = left_area + right_area
-    area_cal = occupancy_estimation(15, 3, 40, 60, true_area)
+    area_cal = occupancy_estimation(13, 3, 40, 60, true_area)
 
-    SOURCE = [roi_points[i] for i in range(4)]
-    SOURCE = np.array(SOURCE)
+    SOURCE = np.array([roi_points[i] for i in range(4)])
 
-    # setup manual --> can auto
+    # Setup manual --> can be automated
     TARGET = np.array([[0, 19],
-                        [0, 0],
-                        [11, 0],
-                        [11, 19]])
+                       [0, 0],
+                       [11, 0],
+                       [11, 19]])
+
+    global view_transformer
+    view_transformer = ViewTransformer(source=SOURCE, target=TARGET)
 
     while True:
+        start = time.time()
         ret, frame = vid.read()
         if not ret:
             break
 
-        pts = np.array(roi_points, np.int32)
-        pts = pts.reshape((-1, 1, 2))
+        pts = np.array(roi_points, np.int32).reshape((-1, 1, 2))
         cv2.polylines(frame, [pts], isClosed=True, color=(0, 0, 255), thickness=2)
-        
-        # inference detection
+
+        # Inference detection
         results = model(frame)
-        data = results.pandas().xyxy[0].to_json(orient="records")  # JSON img1 predictions
+        data = results.pandas().xyxy[0].to_json(orient="records")
         json_data = json.loads(data)
 
         if not json_data:
             continue
 
-        # output has to be N X (x, y, x, y, conf, cls)
+        # Output has to be N x (x, y, x, y, conf, cls)
         num_frame += 1
         detect = []
         for record in json_data:
-            confidence, class_id = round(record['confidence'], 2), record['class']
-            left, top, right, bottom = int(record['xmin']), int(record['ymin']), int(record['xmax']), int(record['ymax'])
-            # cv2.rectangle(frame, (left, top), (right, bottom), colors[class_id], 2)
-            # cv2.putText(frame, "{}_{}".format(class_names[class_id], confidence), 
-            #             (left, top - 2), cv2.FONT_HERSHEY_SIMPLEX, 0.5, colors[class_id], 2)
+            confidence = round(record['confidence'], 2)
+            class_id = record['class']
+            left = int(record['xmin'])
+            top = int(record['ymin'])
+            right = int(record['xmax'])
+            bottom = int(record['ymax'])
             if class_id in tracking_class and confidence >= conf_threshold:
                 detect.append([left, top, right, bottom, confidence, class_id])
 
-        # Check if there are any detections
         detect = np.array(detect)
         if len(detect) > 0:
-            tracks = tracker.update(detect, frame) # --> M X (x, y, x, y, id, conf, cls, ind)
-        # If no detections, make prediction ahead
+            tracks = tracker.update(detect, frame)  # M x (x, y, x, y, id, conf, cls, ind)
         else:
-            detect = np.empty((0, 6))  # empty N X (x, y, x, y, conf, cls)
-            tracks = tracker.update(detect, frame) # --> M X (x, y, x, y, id, conf, cls, ind)
+            detect = np.empty((0, 6))  # Empty N x (x, y, x, y, conf, cls)
+            tracks = tracker.update(detect, frame)  # M x (x, y, x, y, id, conf, cls, ind)
 
-        # tracker.plot_results(im, show_trajectories=True) # auto draw object
-        if (tracks is not None) and (len(tracks)>0):
+        if tracks is not None and len(tracks) > 0:
             # draw_box(frame, tracks, distance_cal, num_frame, area_cal)
             draw_box_2(frame, tracks, distance_cal, num_frame, area_cal, SOURCE, TARGET)
 
-        # break on pressing q or space
+        # Break on pressing q or space
         cv2.imshow('BoxMOT detection', frame)
+        print("time per frame: {}s".format(time.time() - start))
         key = cv2.waitKey(1) & 0xFF
         if key == ord(' ') or key == ord('q'):
             break
@@ -446,7 +453,4 @@ def main(_argv):
     cv2.destroyAllWindows()
 
 if __name__ == '__main__':
-    try:
-        app.run(main)
-    except SystemExit:
-        pass
+    main()
